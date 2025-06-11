afef3c336e2dba97529dff6b30dd4f1c67ab13f4
michael545 <michael.valand@gmail.com>
2025-06-11 18:55:47 +0200

    rebase

--- euphemia_simulation/order_types/standard_orders.py
+++ euphemia_simulation/order_types/standard_orders.py
@@ -78,8 +78,7 @@ class MeritOrder(StepOrder):
 
     def __str__(self):
         return f"MeritOrder ID: {self.order_id}, Zone: {self.bidding_zone}, Side: {self.side}, Price: {self.price}, MON: {self.merit_order_number}"
-
-
+# 2.7. PUNOrder (Discontinued)
 '''
 used to be relevant for the Italian market, but nnow since Jan 2025 it's dicontinued "Prezzo Unico Nazionale" PUN
 '''
